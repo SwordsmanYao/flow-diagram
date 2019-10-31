7b59971a1beb6febd3a7128b0ccff51981a7edaa
SwordsmanYao <swordsman.yao@foxmail.com>
2019-10-31 13:55:59 +0800

    init

--- src/interfaces/flow.ts
+++ src/interfaces/flow.ts
@@ -2,4 +2,38 @@ import { Position } from './generics';
 
 export interface Flow {
   position: Position;
+  nodes: {
+    [id: string]: Node,
+  }
+  links: {
+    [id: string]: Link,
+  }
+}
+
+export interface Node {
+  id: string;
+  type: string;
+  position: Position;
+  ports: {
+    [id: string]: Port;
+  };
+}
+
+export interface Link {
+  id: string;
+  type: string;
+  from: {
+    nodeId: string;
+    portId: string;
+  },
+  to: {
+    nodeId: string;
+    portId: string;
+  }
+}
+
+export interface Port {
+  id: string;
+  type: string;
+  position: Position;
 }
--- src/interfaces/generics.ts
+++ src/interfaces/generics.ts
@@ -2,3 +2,8 @@ export interface Position {
   x: number;
   y: number;
 }
+
+export interface Size {
+  width: number
+  height: number
+}
