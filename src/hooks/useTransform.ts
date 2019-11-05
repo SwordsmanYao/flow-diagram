d5ea43ead885d665d1a3c4b24a21c2b3005d4f18
SwordsmanYao <swordsman.yao@foxmail.com>
2019-11-05 14:42:47 +0800

    完成拖拽 缩放

--- src/hooks/useTransform.ts
+++ src/hooks/useTransform.ts
@@ -1,7 +1,7 @@
 import { RefObject, useEffect, useState } from "react";
 import { Transform } from "../interfaces";
 import { fromEvent } from "rxjs";
-import { scan, map, switchMap, takeUntil } from "rxjs/operators";
+import { map, switchMap, takeUntil } from "rxjs/operators";
 
 /**
  *
@@ -27,56 +27,84 @@ export const useTransform = (
       const mouseUp$ = fromEvent<MouseEvent>(document, "mouseup");
       const wheel$ = fromEvent<WheelEvent>(eventElementRef.current, "wheel");
 
-      mouseDown$
-        .pipe(
-          map(event => {
-            event.preventDefault();
-            event.stopPropagation();
-            const relativeRect = relativeElementRef.current!.getBoundingClientRect();
-            const targetRect = targetElementRef.current!.getBoundingClientRect();
-            return {
-              prePosition: {
-                x: targetRect.left - relativeRect.left,
-                y: targetRect.top - relativeRect.top
-              },
-              downEvent: event
-            };
-          }),
-          switchMap(({ prePosition, downEvent }) => {
-            return mouseMove$.pipe(
-              takeUntil(mouseUp$),
-              map(moveEvent => ({
-                x: prePosition.x + moveEvent.clientX - downEvent.clientX,
-                y: prePosition.y + moveEvent.clientY - downEvent.clientY
-              }))
-            );
-          })
-        )
-        .subscribe(position => {
-          setTransform(preState => ({
-            ...preState,
-            position: {
-              x: position.x / preState.zoom,
-              y: position.y / preState.zoom,
-            }
-          }));
-        });
+      const move$ = mouseDown$.pipe(
+        map(event => {
+          event.preventDefault();
+          event.stopPropagation();
+          const relativeRect = relativeElementRef.current!.getBoundingClientRect();
+          const targetRect = targetElementRef.current!.getBoundingClientRect();
+          return {
+            prePosition: {
+              x: targetRect.left - relativeRect.left,
+              y: targetRect.top - relativeRect.top
+            },
+            downEvent: event
+          };
+        }),
+        switchMap(({ prePosition, downEvent }) => {
+          return mouseMove$.pipe(
+            takeUntil(mouseUp$),
+            map(moveEvent => ({
+              x: prePosition.x + moveEvent.clientX - downEvent.clientX,
+              y: prePosition.y + moveEvent.clientY - downEvent.clientY
+            }))
+          );
+        })
+      );
 
-      wheel$
-        .pipe(
-          map(event => {
-            event.preventDefault();
-            event.stopPropagation();
-            return event.deltaY;
-          }),
-          scan((sum, current) => sum - current * 0.01, initialTransform.zoom)
+      const zoom$ = wheel$.pipe(
+        map(event => {
+          event.preventDefault();
+          event.stopPropagation();
+          const targetRect = targetElementRef.current!.getBoundingClientRect();
+          return {
+            event,
+            targetRect
+          };
+        }),
+        map(
+          ({ event, targetRect }) => {
+            const transformStr = targetElementRef.current!.style.transform;
+            const matchs = transformStr.match(
+              /^scale\(([\s\S]+)\) translate\(([\s\S]+)px, ([\s\S]+)px\)$/
+            ) as any;
+            const preZoom = Number(matchs[1]);
+            const positionX = Number(matchs[2]);
+            const positionY = Number(matchs[3]);
+            const zoom = preZoom + (event.deltaY * 0.01) / 53;
+            const x =
+              ((event.clientX - targetRect.left) * (1 - zoom / preZoom) +
+                positionX * preZoom) /
+              zoom;
+            const y =
+              ((event.clientY - targetRect.top) * (1 - zoom / preZoom) +
+                positionY * preZoom) /
+              zoom;
+            return {
+              zoom,
+              position: {
+                x,
+                y
+              }
+            };
+          }
         )
-        .subscribe(zoom => {
-          setTransform(preState => ({
-            ...preState,
-            zoom
-          }));
-        });
+      );
+
+      move$.subscribe(position => {
+        setTransform(preState => ({
+          ...preState,
+          position: {
+            x: position.x / preState.zoom,
+            y: position.y / preState.zoom
+          }
+        }));
+      });
+
+      zoom$.subscribe(val => {
+        targetElementRef.current!.style.transform = `scale(${val.zoom}) translate(${val.position.x}px, ${val.position.y}px)`;
+        setTransform(val);
+      });
     }
   }, []);
 
